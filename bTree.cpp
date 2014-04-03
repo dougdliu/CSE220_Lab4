bae15999b8dbdb6703ebed62d033faccaf6c4784
dougdliu <dougdliu@gmail.com>
2014-04-03 13:07:05 -0700

    Create bTree.cpp
    This is not finished yet

---
+++ bTree.cpp
@@ -0,0 +1,22 @@
+/*
+ * Created By: Doug Liu
+ * Description: This is the source file for the binary tree class.
+ *
+ */
+ 
+#include "bTree.h"
+
+bTree::bTree() //This is the constructor
+{
+  root = NULL;
+}
+bTree::~bTree() //This is the destructor
+{
+  destroy();
+}
+bTree::add() //This will add the identifier token either to the binary tree or adds page number to the linked list.
+{
+
+
+
+}
