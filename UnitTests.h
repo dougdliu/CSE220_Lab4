333e7a3f639d8cf3f4815a1f8b57396c7497dbe8
Jake Borg <ebmixed@gmail.com>
2014-04-09 01:19:45 -0700

    Testing updates

--- UnitTests.h
+++ UnitTests.h
@@ -29,12 +29,24 @@ class UnitTests {
 		bool testSkipBlanks();
 		bool testSkipComment();
 		bool testGetWord();
-		// TODO the rest of the methods
+		// TODO
 
 		/* bTree tests */
 		bool testAddToTree();
 		bool testDestroyTree();
+		bool testPrintTree();
+
+		/* LinkedList tests */
+		bool testAddToList();
+		bool testPrintList();
+		bool testDeleteList();
+		bool testSet();
+
+		/* Token tests */
+		// Do we need to test getters and setters?
+
 	private:
+		LinkedList lines;
 		bTree tree;
 		Token *a;
 		Token *b;
