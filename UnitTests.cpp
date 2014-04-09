84b62ad8a52ab237194328f3f5f0576c0a911cec
Jake Borg <ebmixed@gmail.com>
2014-04-09 01:20:22 -0700

    Testing updates

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -1,6 +1,7 @@
 #include "UnitTests.h"
 
 UnitTests::UnitTests() {
+	/* bTree stuff */
 	a = new Token();
 	b = new Token();
 	c = new Token();
@@ -9,13 +10,13 @@ UnitTests::UnitTests() {
 	b->setTokenString("b");
 	c->setTokenString("c");
 	d->setTokenString("d");
+}
+/* bTree tests */
+bool UnitTests::testAddToTree() {
 	tree.addToTree(b,1);
 	tree.addToTree(a,0);
 	tree.addToTree(d,3);
 	tree.addToTree(c,2);
-}
-/* bTree tests */
-bool UnitTests::testAddToTree() {
 	if(a != b->getLeftChild()) {
 		printf("addToTree left child");
 		return 0;
@@ -32,9 +33,25 @@ bool UnitTests::testAddToTree() {
 }
 bool UnitTests::testDestroyTree() {
 	tree.destroyTree();
-	if(a != NULL) { return 0; }
-	if(b != NULL) { return 0; }
-	if(c != NULL) { return 0; }
-	if(d != NULL) { return 0; }
+	if(a != NULL) { printf("a not destroyed"); return 0; }
+	if(b != NULL) { printf("b not destroyed"); return 0; }
+	if(c != NULL) { printf("c not destroyed"); return 0; }
+	if(d != NULL) { printf("d not destroyed"); return 0; }
 	return 1;
+}
+/* LinkedList tests */
+bool UnitTests::testAddToList() {
+	lines.addToList(1);
+	lines.addToList(1);
+	lines.addToList(2);
+	lines.addToList(3);
+	if(lines.getLine() != 1) { printf("depth 0 not set"); return 0; }
+	if(lines.getNext()->getLine() != 1) { printf("depth 1 not set"); return 0; }
+	if(lines.getNext()->getNext()->getLine() != 2) { printf("depth 2 not set"); return 0; }
+	if(lines.getNext()->getNext()->getNext()->getLine() != 3) { printf("depth 3 not set"); return 0; }
+	return 1;
+}
+bool UnitTests::testDeleteList() {
+	if(lines.getLine() != NULL) { printf("delete list not working"); return 0;}
+	return 1; //? How to test inner nodes?
 }
