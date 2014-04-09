6e54fa5f8129a8acca59815b752caf8f26f441e2
achasse <achasse@asu.edu>
2014-04-09 08:41:29 -0700

    Added file information header + comments

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,30 +1,38 @@
 /*
- *CSE 220 Lab 4
- *LinkedList.cpp to implement LinkedList object
+ *Authors: Aaron Chasse, Jake Borg, Deming Liu
+ *CSE 220 - Lab 4
+ *04/09/2014
  *
+ *Program Description:
+ *LinkedList.cpp to implement LinkedList object. Each Token object will contain
+ *one LinkedList object, and form a linked list representing the line numbers where
+ *the token was found in the input file.
  *
 */
+
+
 #include "LinkedList.h"
 
 
-
+//Default constructor sets line to zero and pointer to next LinkedList object to NULL
 LinkedList::LinkedList()
 {
 	line = 0;
 	nextLine = NULL;	
-	
-	//default constructor
 }
-LinkedList::~LinkedList()
-{
-	//destructor
-}
+//Free the heap memory for the LinkedList object
+LinkedList::~LinkedList(){}
+
+//This method will allocate memory on the heap for a new LinkedLit object
+//to be part of the linked list of line numbers for the Token it is part of.
 void LinkedList::addToList(int newLine)
 {
-	
+	//LinkedList pointers for iterating through the list
 	LinkedList *current;
 	LinkedList *previous;
 	
+	//Create new instance of LinedList called newNode, set its line number to the incoming
+	//integer
 	LinkedList *newNode = new LinkedList;
 	newNode->line = newLine;
 	
@@ -48,20 +56,15 @@ void LinkedList::addToList(int newLine)
 		previous->nextLine = newNode;
 		newNode->nextLine = current;
 		
-	}
+	}//end else block
 	
-	
-	
-	
-	
-}
+}//end method addToList
+
+//Traverse through the list and print the line numbers
 void LinkedList::printList()
 {
-	//traverse through the list and print the line numbers
 	
 	LinkedList *current;
-	
-	
 	current = this;
 	
 	while(current != NULL)
@@ -72,7 +75,9 @@ void LinkedList::printList()
 		current = current->nextLine;
 	}
 	
-}
+}//end method printList
+
+//Delete the list of LinkedLst objects
 void LinkedList::deleteList()
 {
 	LinkedList *temp;
@@ -82,7 +87,6 @@ void LinkedList::deleteList()
 		temp = this->nextLine;
 		this->nextLine = temp->nextLine;
 		delete temp;	
-		
 	}
 }
 void LinkedList::set(int line_number)
