7db406d32105e0a8c54788346348264c2e528c25
Jake Borg <ebmixed@gmail.com>
2014-04-08 18:53:51 -0700

    Pointer change

--- Scanner.cpp
+++ Scanner.cpp
@@ -29,7 +29,7 @@ const RwStruct rw_table[9][10] = {
 Scanner::Scanner(FILE *source_file, char source_name[], char date[], Print printer) : print(printer)
 {
     src_file = source_file;
-	*line_ptr = NULL;
+    line_ptr = NULL;
     strcpy(src_name, source_name);
     strcpy(todays_date, date);
     
