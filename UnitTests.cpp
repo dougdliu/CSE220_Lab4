b2da2be3228032bdc1e415b3d44a4440a353f5e2
Jake Borg <ebmixed@gmail.com>
2014-04-09 17:40:59 -0700

    Update UnitTests.cpp

--- UnitTests.cpp
+++ UnitTests.cpp
@@ -56,3 +56,55 @@ bool UnitTests::testDeleteList() {
 	if(lines.getLine() != NULL) { printf("delete list not working"); return 0;}
 	return 1; //? How to test inner nodes?
 }
+bool UnitTests::testSet() {
+	lines.set(17);
+	if(lines.getLine() != 17) {
+		printf("line not set to 17");
+		return 0;
+	}
+	return 1;
+}
+
+/* Scanner tests */
+bool UnitTests::testGetToken(char source_name[], char date[], Print printer) {
+	FILE *file;
+	Token *token = NULL;
+	file = fopen("UnitTests.txt" , "r");
+	Scanner scanner(file, source_name, date, printer);
+	token = scanner.getToken();
+	if(token->getTokenString().compare("identifier") != 0) {
+		printf("getToken fails to get identifiers\n");
+		return 0;
+	}
+	token = scanner.getToken();
+	if(token->getTokenString().compare("one") != 0) {
+		printf("getToken fails to get second token\n");
+		return 0;
+	}
+	token = scanner.getToken();
+	if(token->getTokenString().compare("two") != 0) {
+		printf("getToken fails to get third token\n");
+		return 0;
+	}
+	token = scanner.getToken();
+	if(token->getTokenString().compare("newline") != 0) {
+		printf("getToken fails for multiple lines\n");
+		return 0;
+	}
+	token = scanner.getToken();
+	if(token->getStringLiteral().compare("'string'") != 0) {
+		printf("getToken fails for to get strings\n");
+		return 0;
+	}
+	token = scanner.getToken();
+	if(token->getIntLiteral() != 1234) {
+		printf("getToken fails to get integers\n");
+		return 0;
+	}
+	token = scanner.getToken();
+	if(std::abs(token->getRealLiteral()-5.67) >= .01*5.67) {
+		printf("getToken fails to get floating point numbers\n");
+		return 0;
+	}
+	return 1;
+}
